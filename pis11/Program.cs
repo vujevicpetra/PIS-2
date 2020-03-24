59c4347d276b3c2a892180062ecd5984df1463d6
Josipa Jukić <jjukic01@fesb.hr>
2020-03-24 18:32:09 +0100

    Add project files.

---
+++ pis11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pis11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
