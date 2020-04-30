e91c77c05e41cb68856e9b080b780e929c954680
vujevicpetra <63189451+vujevicpetra@users.noreply.github.com>
2020-04-30 13:46:56 +0200

    Update Program.cd
    Dodala sam komentar.

--- pis11/Program.cs
+++ pis11/Program.cs
@@ -8,6 +8,7 @@ namespace pis11
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Josipa je najlosiji student na fesbu"); //ovo je editano od strane suradnika
+            Console.WriteLine("Josipa je najbolji student na fesbu");
         }
     }
 
